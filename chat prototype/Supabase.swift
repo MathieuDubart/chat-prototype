afe397687368b61d056d10e13c8d8feef01ed3a7
Mathieu Dubart <mathieu.dubart@icloud.com>
2026-03-03 15:53:36 +0100

    :

---
+++ chat prototype/Supabase.swift
@@ -0,0 +1,14 @@
+//
+//  Supabase.swift
+//  chat prototype
+//
+//  Created by Mathieu Dubart on 03/03/2026.
+//
+
+import Supabase
+import Foundation
+
+let supabase = SupabaseClient(
+    supabaseURL: URL(string: "Yhttps://zdyhaqmggoztwtlqhhma.supabase.co")!,
+    supabaseKey: "sb_publishable_ghqr60ROBbfa5BNma3VLzw_0WcsCGvv"
+)
