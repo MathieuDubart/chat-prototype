d3561b3fbf770cbce7d9753303eb30cc3909c0c1
Mathieu Dubart <mathieu.dubart@icloud.com>
2026-03-03 15:27:09 +0100

    Initial Commit

---
+++ chat prototype/chat_prototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  chat_prototypeApp.swift
+//  chat prototype
+//
+//  Created by Mathieu Dubart on 03/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct chat_prototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
